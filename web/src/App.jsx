a0a8c06bf878e1fb34a584ac97ba2b0f7559d581
hiromikoreyes <114784445+hiromikoreyes@users.noreply.github.com>
2024-02-21 12:53:40 -0500

    Added router to App.jsx + bootstrap dependency

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,7 +1,17 @@
+import { BrowserRouter } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
+import 'bootstrap/dist/css/bootstrap.css';
+
 function App() {
 
   return (
     <>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
