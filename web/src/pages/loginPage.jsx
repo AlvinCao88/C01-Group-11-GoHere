3e63d98f0333e1ed646c0be69af557d73fbec19e
hiromikoreyes <114784445+hiromikoreyes@users.noreply.github.com>
2024-02-21 13:02:45 -0500

    styled LoginPage.jsx

--- web/src/pages/loginPage.jsx
+++ web/src/pages/loginPage.jsx
@@ -3,23 +3,28 @@ import Form from 'react-bootstrap/Form';
 
 function loginPage() {
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+    <div className='d-flex justify-content-center' style={{maxWidth: '100vw', width: '100vw', height: '100vh'}}>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+    <div style={{width: '500px', marginTop: '8rem'}}>
+        <h1>GoHere Administrator Site</h1>
+        <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+            Submit
+        </Button>
+        </Form>
+    </div>
+    </div>
+
+
   );
 }
 
