f97468c08ea7e85024faaa5ad86cee05f9aed55a
hiromikoreyes <114784445+hiromikoreyes@users.noreply.github.com>
2024-02-25 18:17:21 -0500

    Added /loginUser POST request

--- server/routes/admin.route.js
+++ server/routes/admin.route.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import jwt from "jsonwebtoken";
 import AdminController from "../controller/admin.controller.js";
+import bcrypt from 'bcrypt';
+import { Admin } from "mongodb";
 
 const router = Router();
 // Define Endpoints Here
@@ -10,26 +12,44 @@ router.post("/registerUser", async (req, res) => {
     try {
       const { email, password } = req.body;
   
-      // Basic body request check
       if (!email || !password) {
         return res
           .status(400)
           .json({ error: "Email and password both needed to register." });
       }
   
-      // Checking if username does not already exist in database
       const existingUser = await AdminController.getUser();
       if (existingUser) {
         return res.status(400).json({ error: "Email already exists." });
       }
   
-      // Creating hashed password (search up bcrypt online for more info)
-      // and storing user info in database
-      AdminController.createUser(email, password)
+      AdminController.createUser(email, password);
   
-      // Returning JSON Web Token (search JWT for more explanation)
       const token = jwt.sign({ email }, "secret-key", { expiresIn: "1h" });
       res.status(201).json({ response: "User registered successfully.", token });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+
+  router.post("/loginUser", async (req, res) => {
+    try {
+      const { email, password } = req.body;
+  
+      if (!email || !password) {
+        return res
+          .status(400)
+          .json({ error: "Email and password both needed to login." });
+      }
+  
+      const user = await AdminController.getUser(email);
+  
+      if (user && (await bcrypt.compare(password, user.password))) {
+        const token = jwt.sign({ email }, "secret-key", { expiresIn: "1h" });
+        res.json({ response: "User logged in succesfully.", token: token }); 
+      } else {
+        res.status(401).json({ error: "Authentication failed." });
+      }
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
