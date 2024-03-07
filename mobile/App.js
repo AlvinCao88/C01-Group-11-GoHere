076d5405f262bc4d98e70de2e8f84e3550d5b64e
yong1le <ygle.he@gmail.com>
2024-03-06 20:50:03 -0500

    add primisc dependencies

--- mobile/App.js
+++ mobile/App.js
@@ -2,41 +2,49 @@ import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import { PrismicProvider } from "@prismicio/react";
+import { client } from "./prismic";
 
 import MapScreen from "./src/screens/MapScreen";
+import NewsScreen from "./src/screens/NewsScreen";
 import CardScreen from "./src/screens/CardScreen";
 import TestScreen from "./src/screens/TestScreen";
-import DonateScreen from "./src/screens/DonationScreen";
-import AddWashroomsScreen from "./src/screens/AddWashroomScreen";
 import DonationScreen from "./src/screens/DonationScreen";
 import AddWashroomScreen from "./src/screens/AddWashroomScreen";
-import { StatusBar } from "expo-status-bar";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <StatusBar/>
-      <Tab.Navigator initialRouteName="Map">
-        <Tab.Screen
-          name="Map"
-          component={MapScreen}
-          options={{ headerShown: false }}
-        />
-        <Tab.Screen
-          name="Card"
-          component={CardScreen}
-          options={{ headerShown: false }}
-        />
-        <Tab.Screen
-          name="Info"
-          component={InfoNavigator}
-          options={{ headerShown: false }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <PrismicProvider client={client}>
+      <NavigationContainer>
+        <StatusBar />
+        <Tab.Navigator initialRouteName="Map">
+          <Tab.Screen
+            name="Map"
+            component={MapScreen}
+            options={{ headerShown: false }}
+          />
+          <Tab.Screen
+            name="News"
+            component={NewsScreen}
+            options={{ headerShown: false }}
+          />
+          <Tab.Screen
+            name="Card"
+            component={CardScreen}
+            options={{ headerShown: false }}
+          />
+          <Tab.Screen
+            name="Info"
+            component={InfoNavigator}
+            options={{ headerShown: false }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </PrismicProvider>
   );
 }
 
---
+++ mobile/src/screens/NewsScreen.js
@@ -0,0 +1,14 @@
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const NewsScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>
+        Hello
+      </Text>
+    </SafeAreaView>
+  )
+}
+
+export default NewsScreen
