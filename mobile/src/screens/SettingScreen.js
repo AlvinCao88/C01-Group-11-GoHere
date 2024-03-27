53c9c58c9eb69009c8e2027bff46ed91db9936d2
MirrorLimit <113953454+MirrorLimit@users.noreply.github.com>
2024-03-27 17:24:45 -0400

    Changed background colour to be white, consistent with everything else.

--- mobile/src/screens/SettingScreen.js
+++ mobile/src/screens/SettingScreen.js
@@ -11,132 +11,132 @@ export default function SettingScreen ({navigation}) {
     const supportEmailSubject = "support%20request";
 
     return (
-        <ScrollView>
-        <SafeAreaView style={styles.container}>
-            <View style={styles.headerContainer}>
-                <Text style={[styles.titleText, { color: "#DA5C59" }]}>
-                    Settings
+        <ScrollView style={styles.scrollViewStyle}>
+            <SafeAreaView style={styles.container}>
+                <View style={styles.headerContainer}>
+                    <Text style={[styles.titleText, { color: "#DA5C59" }]}>
+                        Settings
+                    </Text>
+                </View>
+
+                <TouchableOpacity
+                    style={styles.button}
+                    activeOpacity={0.3}
+                    onPress={() => navigation.navigate("ProfileScreen")}
+                >
+                    <View style={styles.arrow}>
+                        <Text style={styles.buttonText}>
+                                My profile
+                        </Text>
+                        <Ionicons name="chevron-forward-outline" size={16}></Ionicons>
+                    </View>
+                </TouchableOpacity>
+
+                <Text style={styles.headerText}>
+                    APP SETTINGS
                 </Text>
-            </View>
+                <View style={styles.dividingLine}></View>
 
-            <TouchableOpacity
-                style={styles.button}
-                activeOpacity={0.3}
-                onPress={() => navigation.navigate("ProfileScreen")}
-            >
-                <View style={styles.arrow}>
-                    <Text style={styles.buttonText}>
-                            My profile
+                <TouchableOpacity
+                    style={styles.button}
+                    activeOpacity={0.3}
+                    onPress={() => Linking.openSettings()}
+                >
+                    <View style={styles.arrow}>
+                        <Text style={styles.buttonText}>
+                            Location Permission
+                        </Text>
+                        <Ionicons name="chevron-forward-outline" size={16}></Ionicons>
+                    </View>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.button} activeOpacity={1}>
+                    <View style={styles.arrow}>
+                        <Text style={styles.buttonText}>Analytics</Text>
+                        <Switch
+                            value={analytics}
+                            onValueChange={() => setAnalytics(!analytics)}
+                            trackColor={{false: "#767577", true: "#DA5C59"}}
+                            thumbColor={analytics ? "white" : "#BEBEBE"}
+                        ></Switch>
+                    </View>
+                    <Text style={styles.smallText}>
+                        Enable analytics to send anonymous usage to support app improvements.
                     </Text>
-                    <Ionicons name="chevron-forward-outline" size={16}></Ionicons>
-                </View>
-            </TouchableOpacity>
+                </TouchableOpacity>
 
-            <Text style={styles.headerText}>
-                APP SETTINGS
-            </Text>
-            <View style={styles.dividingLine}></View>
-
-            <TouchableOpacity
-                style={styles.button}
-                activeOpacity={0.3}
-                onPress={() => Linking.openSettings()}
-            >
-                <View style={styles.arrow}>
-                    <Text style={styles.buttonText}>
-                        Location Permission
-                    </Text>
-                    <Ionicons name="chevron-forward-outline" size={16}></Ionicons>
-                </View>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.button} activeOpacity={1}>
-                <View style={styles.arrow}>
-                    <Text style={styles.buttonText}>Analytics</Text>
-                    <Switch
-                        value={analytics}
-                        onValueChange={() => setAnalytics(!analytics)}
-                        trackColor={{false: "#767577", true: "#DA5C59"}}
-                        thumbColor={analytics ? "white" : "#BEBEBE"}
-                    ></Switch>
-                </View>
-                <Text style={styles.smallText}>
-                    Enable analytics to send anonymous usage to support app improvements.
+                <Text style={styles.headerText}>
+                    PRIVACY AND TERMS
                 </Text>
-            </TouchableOpacity>
+                <View style={styles.dividingLine}></View>
 
-            <Text style={styles.headerText}>
-                PRIVACY AND TERMS
-            </Text>
-            <View style={styles.dividingLine}></View>
+                <TouchableOpacity
+                    style={styles.button}
+                    activeOpacity={0.3}
+                    onPress={() => Linking.openURL("https://crohnsandcolitis.ca/Privacy-Policy")}
+                >
+                    <View style={styles.arrow}>
+                        <Text style={styles.buttonText}>
+                            Privacy Policy
+                        </Text>
+                        <Ionicons name="chevron-forward-outline" size={16}></Ionicons>
+                    </View>
+                </TouchableOpacity>
 
-            <TouchableOpacity
-                style={styles.button}
-                activeOpacity={0.3}
-                onPress={() => Linking.openURL("https://crohnsandcolitis.ca/Privacy-Policy")}
-            >
-                <View style={styles.arrow}>
-                    <Text style={styles.buttonText}>
-                        Privacy Policy
-                    </Text>
-                    <Ionicons name="chevron-forward-outline" size={16}></Ionicons>
-                </View>
-            </TouchableOpacity>
+                <Text style={styles.headerText}>
+                    SUPPORT
+                </Text>
+                <View style={styles.dividingLine}></View>
 
-            <Text style={styles.headerText}>
-                SUPPORT
-            </Text>
-            <View style={styles.dividingLine}></View>
+                <TouchableOpacity
+                    style={styles.button}
+                    activeOpacity={0.3}
+                    onPress={() => Linking.openURL(`mailto:${supportEmailAddress}?subject=${supportEmailSubject}`)}
+                >
+                    <View style={styles.arrow}>
+                        <Text style={styles.buttonText}>
+                            Request Support
+                        </Text>
+                        <Ionicons name="chevron-forward-outline" size={16}></Ionicons>
+                    </View>
+                </TouchableOpacity>
 
-            <TouchableOpacity
-                style={styles.button}
-                activeOpacity={0.3}
-                onPress={() => Linking.openURL(`mailto:${supportEmailAddress}?subject=${supportEmailSubject}`)}
-            >
-                <View style={styles.arrow}>
-                    <Text style={styles.buttonText}>
-                        Request Support
-                    </Text>
-                    <Ionicons name="chevron-forward-outline" size={16}></Ionicons>
-                </View>
-            </TouchableOpacity>
+                <TouchableOpacity style={styles.button} activeOpacity={1}>
+                    <View style={styles.arrow}>
+                        <Text style={styles.buttonText}>
+                            Version
+                        </Text>
+                        <Text style={styles.versionText}>
+                            1.0.0
+                        </Text>
+                    </View>
+                </TouchableOpacity>
+        
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => navigation.navigate("AddWashrooms")}
+                >
+                    <View style={styles.arrow}>
+                        <Text style={styles.buttonText}>
+                            Add a Washroom
+                        </Text>
+                        <Ionicons name="chevron-forward-outline" size={16}></Ionicons>
+                    </View>
+                </TouchableOpacity>
 
-            <TouchableOpacity style={styles.button} activeOpacity={1}>
-                <View style={styles.arrow}>
-                    <Text style={styles.buttonText}>
-                        Version
-                    </Text>
-                    <Text style={styles.versionText}>
-                        1.0.0
-                    </Text>
-                </View>
-            </TouchableOpacity>
-    
-            <TouchableOpacity
-                style={styles.button}
-                onPress={() => navigation.navigate("AddWashrooms")}
-            >
-                <View style={styles.arrow}>
-                    <Text style={styles.buttonText}>
-                        Add a Washroom
-                    </Text>
-                    <Ionicons name="chevron-forward-outline" size={16}></Ionicons>
-                </View>
-            </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => navigation.navigate("AddBusinesses")}
+                >
+                    <View style={styles.arrow}>
+                        <Text style={styles.buttonText}>
+                            Add a Business
+                        </Text>
+                        <Ionicons name="chevron-forward-outline" size={16}></Ionicons>
+                    </View>
+                </TouchableOpacity>
 
-            <TouchableOpacity
-                style={styles.button}
-                onPress={() => navigation.navigate("AddBusinesses")}
-            >
-                <View style={styles.arrow}>
-                    <Text style={styles.buttonText}>
-                        Add a Business
-                    </Text>
-                    <Ionicons name="chevron-forward-outline" size={16}></Ionicons>
-                </View>
-            </TouchableOpacity>
-
-        </SafeAreaView>
+            </SafeAreaView>
         </ScrollView>
     );
 }
@@ -223,5 +223,9 @@ const styles = StyleSheet.create({
         marginBottom: 10,
         color: "#DA5C59",
         paddingHorizontal: 20
+    },
+
+    scrollViewStyle: {
+        backgroundColor: "#ffffff"
     }
 })
