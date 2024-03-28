b5b3b8aa33fafffe36f13715fac7a8183282225d
yong1le <ygle.he@gmail.com>
2024-03-28 14:32:42 -0400

    move utlity functions to their own file

--- server/testing/business.test.js
+++ server/testing/business.test.js
@@ -1,40 +1,5 @@
 const SERVER_URL = "http://localhost:8000";
-const { v4: uuidv4 } = require("uuid");
-
-async function getToken() {
-  const email = `${uuidv4()}@email.com`;
-  const password = `${uuidv4()}`;
-
-  const signUpRes = await fetch(`${SERVER_URL}/admin/registerUser`, {
-    method: "POST",
-    mode: "cors",
-    cache: "no-cache",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      email,
-      password,
-      registrationId: "super secret 123",
-    }),
-  });
-
-  const loginRes = await fetch(`${SERVER_URL}/admin/loginUser`, {
-    method: "POST",
-    mode: "cors",
-    cache: "no-cache",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      email,
-      password,
-    }),
-  });
-
-  const loginResBody = await loginRes.json();
-  return loginResBody.token;
-}
+const { getToken } = require("./utils")
 
 test("/admin/isAdmin", async () => {
   const token = await getToken();
